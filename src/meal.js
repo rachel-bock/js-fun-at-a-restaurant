d4905168662aebd6feb21e9e4157020c54ee4904
Rachel Bock <rachel.online@gmail.com>
2022-03-24 18:24:02 -0600

    Add meal.js functionality - Iteration 1

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`;
 }
 
+function createMenuItem(name, price, type){
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  }
+  return menuItem;
+}
+
+function addIngredients(ingredient, ingredients){
+
+  //If an ingredient exists in the array, do nothing.
+  if (ingredients.includes(ingredient)) {
+    return ingredients;
+  // Otherwise, add the ingredient to the array.
+  } else {
+    ingredients.push(ingredient);
+  }
+}
+
+function formatPrice(initialPrice){
+    return `$${initialPrice}`;
+}
+
+function decreasePrice(price){
+    return price * .9;
+}
+
+function createRecipe(title, ingredients, menuItemType){
+  return {
+      title: title,
+      ingredients: ingredients,
+      type: menuItemType,
+  };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
